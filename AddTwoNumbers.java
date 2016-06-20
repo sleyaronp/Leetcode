3839c885ca3f59e6ff296a03eb30a76bab3baa05
sleyaronp <almarover@gmail.com>
2016-06-20 17:03:30 -0400

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+ public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       int carry = 0;
+       ListNode temp = new ListNode(0);
+       ListNode p1 = l1, p2 = l2, p3 = temp;
+       
+      while(p1 != null || p2 != null){
+      if(p1 != null){
+        carry += p1.val;
+        p1 = p1.next;
+      }
+      if(p2 != null){
+        carry += p2.val;
+        p2 = p2.next;
+      }
+      
+      p3.next = new ListNode(carry%10); //store the unit digit, use the 
+      p3 = p3.next;
+      carry /= 10; //store the carry and do the next round caculation
+      }
+      
+      if(carry != 0)
+      p3.next = new ListNode(1); //add a new digit;
+      
+      return temp.next;
+    }
+ }
