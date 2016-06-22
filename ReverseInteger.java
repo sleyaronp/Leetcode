7453846cfc99ed2b2e2577ce5c598b3639ead340
sleyaronp <almarover@gmail.com>
2016-06-21 20:25:10 -0400

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int reverse(int x) {
+       long result = 0;
+       while(x != 0){
+         result = result*10 + x%10;
+         x /= 10;
+       }
+       
+       if( result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){  //overflow
+            return 0;
+       }else{
+            return (int) result;
+        }
+    }
+}
