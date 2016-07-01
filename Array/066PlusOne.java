c9c7174baf9c084deed0e5a8a1a73b6618bfe555
sleyaronp <almarover@gmail.com>
2016-07-01 13:17:06 -0400

    Create 066PlusOne.java

---
+++ Array/066PlusOne.java
@@ -0,0 +1,21 @@
+//Given a non-negative number represented as an array of digits, plus one to the number.
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+      int carry = 1; //add carry directly
+      
+      for(int i = digits.length - 1; i>=0; i--){
+        if(digits[i] < 9){
+          digits[i] += carry;
+          return digits; //return result here if no carry
+        }else if(digits[i] == 9){
+          digits[i] = 0;
+        }
+      }
+      
+      //add a new digit ahead
+      int[] result = new int[digits.length+1];
+      result[0] = 1;
+      return result;
+    }
+  }
