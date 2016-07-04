6e440c984bc94e21370c262fe84f121dc9c3b961
sleyaronp <almarover@gmail.com>
2016-07-03 22:40:10 -0400

    Update 090SubsetsII.java

--- DepthFirstSearch/090SubsetsII.java
+++ DepthFirstSearch/090SubsetsII.java
@@ -1,6 +1,24 @@
 public class Solution {
   public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    List<Integer> item = new ArrayList<Integer>();
+        
+    if(nums.length == 0 || nums == null) return result;
+      Arrays.sort(nums); 
+        
+      helper(result, item, nums, 0);
+      return result;
+    }
     
-  }
+  public void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int start){
+    result.add(new ArrayList<Integer> (list));
+    
+    for(int i=start; i<nums.length; i++){
+      if(i>start && nums[i] == nums[i-1]) continue; //skip the duplicates
+        list.add(nums[i]);
+        helper(result, list, nums, i+1);
+        list.remove(list.size()-1);
+      }
+    }
 }
 
