765c6c9f360bbf95d1684f8e8c1bab738518f9c2
sleyaronp <almarover@gmail.com>
2016-07-05 19:41:34 -0400

    Create 344ReverseString.java

---
+++ String/344ReverseString.java
@@ -0,0 +1,13 @@
+public class Solution {
+  public String reverseString(String s) {
+    if(s==null || s.length() == 0) return "";
+        
+    StringBuilder result = new StringBuilder();
+        
+    for(int i=s.length()-1; i>=0; i--){
+      result.append(s.charAt(i));
+    }
+        
+    return result.toString();
+  }
+}
